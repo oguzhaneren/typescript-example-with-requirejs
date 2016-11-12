bef786efd293f2ddc33d14c9a1dcc5ff744b9003
Oğuzhan Eren <oguzh4n@gmail.com>
2016-11-12 23:55:37 +0300

    moved viewmodel to feature folder

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,16 +1,10 @@
 import * as ko from "knockout";
 import * as $ from "jquery";
 import * as bs from "bootstrap";
+import HelloViewModel = require("./hello/HelloViewModel")
 
-class HelloViewModel {
-    language: KnockoutObservable<string>
-    framework: KnockoutObservable<string>
 
-    constructor(language: string, framework: string) {
-        this.language = ko.observable(language);
-        this.framework = ko.observable(framework);
-    }
-}
+
 
 ko.applyBindings(new HelloViewModel("TypeScript", "Knockout"));
 var bootstra = bs; // for loading bootstrap
